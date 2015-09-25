d9e3d225b801526a6d78ff516316d47fb1a63432
Vladislav <13thmerlin@gmail.com>
2015-09-25 21:35:48 +0300

    add token response

--- Model/Response/TokenResponse.php
+++ Model/Response/TokenResponse.php
@@ -5,8 +5,83 @@ namespace Clarity\YandexOAuthBundle\Model\Response;
 use JMS\Serializer\Annotation as Serializer;
 
 /**
+ * Class TokenResponse
+ *
+ * @author Vladislav Shishko <13thMerlin@gmail.com>
  * @author varloc2000 <varloc2000@gmail.com>
  */
 class TokenResponse extends BaseResponse
 {
+    /**
+     * @var string
+     *
+     * @Serializer\SerializedName("access_token")
+     * @Serializer\Type("string")
+     */
+    protected $token;
+
+    /**
+     * @var string*
+     *
+     * @Serializer\SerializedName("token_type")
+     * @Serializer\Type("string")
+     *
+     */
+    protected $type;
+
+    /**
+     * @var integer
+     *
+     * @Serializer\SerializedName("expires_in")
+     * @Serializer\Type("integer")
+     */
+    protected $expiresIn;
+
+    /**
+     * @return string
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    /**
+     * @param string $token
+     */
+    public function setToken($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param string $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * @return int
+     */
+    public function getExpiresIn()
+    {
+        return $this->expiresIn;
+    }
+
+    /**
+     * @param int $expiresIn
+     */
+    public function setExpiresIn($expiresIn)
+    {
+        $this->expiresIn = $expiresIn;
+    }
 }
